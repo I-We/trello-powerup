aea2ff6fa2aed81804fc0b232962137d8dbb54e9
Loïc Weinhard <loic.weinhard@iwecloud.com>
2024-07-26 15:07:25 +0200

    fix: show card button

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const t = TrelloPowerUp.iframe();
-
-const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
-
-const onBtnClick = function (t, opts) {
-  return t.popup({
-    title: 'Lancer une preview',
-    url: './index.html', // URL of the popup
-    height: 200
-  });
-};
-
-window.TrelloPowerUp.initialize({
-  'card-buttons': function (t, opts) {
-    return [{
-      icon: GRAY_ICON,
-      text: 'Open Popup',
-      callback: onBtnClick,
-      condition: 'signedIn'
-    }];
-  }
+TrelloPowerUp.initialize({
+  "card-buttons": function (t, options) {
+    return [
+      {
+        icon: "https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421",
+        text: "Lancer une preview",
+        callback: function (t) {
+          return t.popup({
+            title: "Lancer une preview",
+            url: "preview.html",
+          });
+        },
+      },
+    ];
+  },
 });
