840cd492ab9df99c1349c7268c0babbed1b99c4c
Loïc Weinhard <loic.weinhard@iwecloud.com>
2024-07-26 14:55:55 +0200

    fix button

--- index.js
+++ index.js
@@ -1,42 +1,22 @@
 const t = TrelloPowerUp.iframe();
 
-var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
+const GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
-var onBtnClick = function (t, opts) {
-  console.log('Someone clicked the button');
+const onBtnClick = function (t, opts) {
+  return t.popup({
+    title: 'Lancer une preview',
+    url: './index.html', // URL of the popup
+    height: 200
+  });
 };
 
 window.TrelloPowerUp.initialize({
   'card-buttons': function (t, opts) {
     return [{
-      // usually you will provide a callback function to be run on button click
-      // we recommend that you use a popup on click generally
-      icon: GRAY_ICON, // don't use a colored icon here
+      icon: GRAY_ICON,
       text: 'Open Popup',
       callback: onBtnClick,
-      condition: 'edit'
-    }, {
-      // but of course, you could also just kick off to a url if that's your thing
-      icon: GRAY_ICON,
-      text: 'Just a URL',
-      condition: 'always',
-      url: 'https://developer.atlassian.com/cloud/trello',
-      target: 'Trello Developer Site' // optional target for above url
+      condition: 'signedIn'
     }];
   }
-});
-
-t.render(function (secret, options) {
-  document.getElementById('submit').addEventListener('click', function () {
-    const ft = document.getElementById('ft').checked;
-    const mabl = document.getElementById('mabl').checked;
-
-    // Construct data to send (if needed)
-    const data = {
-      ft: ft,
-      mabl: mabl
-    };
-
-    t.closePopup();
-  });
 });
