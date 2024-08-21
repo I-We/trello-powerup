dbef99b370846c3b580e5f82692c3d6895470185
Loïc Weinhard <loic.weinhard@iwecloud.com>
2024-08-21 17:51:09 +0200

    feat(JenkinsBadge): add url

--- index.js
+++ index.js
@@ -85,6 +85,7 @@ function generateStatusBadges(mergeRequest, branchName) {
           title: `${mergeRequest.name} - Jenkins`,
           text: body.message,
           color: body.color,
+          url: body.url,
           refresh: 10
         }
       }
@@ -97,6 +98,7 @@ function generateStatusBadges(mergeRequest, branchName) {
           title: `${mergeRequest.name} - GitLab`,
           text: body.message,
           color: body.color,
+          url: body.url,
           refresh: 10
         }
       }
