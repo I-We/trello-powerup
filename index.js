5c60fdf2e52be10c9fa838ab24ea81db2629b6f2
Loïc Weinhard <loic.weinhard@iwecloud.com>
2024-11-07 14:54:59 +0100

    feat: implement launch pipelines Jenkins button

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ TrelloPowerUp.initialize({
             url,
             title
           ),
-          await generatePatchedVersionsButton(branchName)
+          await generatePatchedVersionsButton(branchName),
+          await generateLaunchPipelinesButton(branchName)
         ];
 
         return buttons.filter(Boolean);
@@ -168,13 +169,10 @@ function sanitize(str) {
   return str.replace(/%E2%80%8C/g, "").trim();
 }
 
-async function generateLaunchPipelinesButton(mergeRequests, branchName) {
-  const mergeRequestsWithPipeline = mergeRequests
-    .filter((mr) => !NO_PIPELINE_PROJECTS.includes(mr.name))
-    .map((mr) => mr.name)
-    .join(",");
-
-  const queryParams = `branch=${branchName}&repositories=${mergeRequestsWithPipeline}`;
+async function generateLaunchPipelinesButton(branchName) {
+  const params = new URLSearchParams({branch: branchName});
+const response = await fetch(`https://n8n.tools.i-we.io/webhook/92beefb2-760b-4a3a-837d-5788a2c340e7?${params.toString()}`);
+const repositories = await response.json();
 
   return {
     icon: IWE_LOGO,
@@ -182,7 +180,7 @@ async function generateLaunchPipelinesButton(mergeRequests, branchName) {
     callback: function (t) {
       return t.popup({
         title: "iWE - Jenkins",
-        url: `jenkins.html?${queryParams}`,
+        url: `jenkins.html?branch=${branchName}&repositories=${repositories.join(',')}`,
       });
     },
   };
