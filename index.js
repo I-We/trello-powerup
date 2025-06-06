e4386a5c07b56b5091be807a364cde8924a84b49
Loïc Weinhard <loic.weinhard@iwecloud.com>
2025-06-06 14:11:04 +0200

    fix: webhook URLs

--- index.js
+++ index.js
@@ -242,7 +242,7 @@ function sanitize(str) {
 
 async function generateLaunchPipelinesButton(branchName) {
   const params = new URLSearchParams({branch: branchName});
-  const response = await fetch(`https://n8n.tools.i-we.io/webhook/92beefb2-760b-4a3a-837d-5788a2c340e7?${params.toString()}`);
+  const response = await fetch(`https://n8n.tools.i-we.io/webhook/bouton-jenkins?${params.toString()}`);
   const repositories = await response.json();
 
   return {
